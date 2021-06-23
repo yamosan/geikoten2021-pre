12156e6bd854b5cb52564c01b80ac61c9c6617e9
yamori-masato <yamorimasato@gmail.com>
2021-06-23 23:13:42 +0900

    add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#D3D977",
+        secondary: "#3A9151",
+        bg: "#FFFAEC",
+        white: "#FFFFFF",
+        black: "#504746",
+        gray: "#BFBFBF",
+        "light-gray": "#DFDFDF",
+      }
+    }
   },
   variants: {
     extend: {},
