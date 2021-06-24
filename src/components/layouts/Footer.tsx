ca74749a25ca2c92071df63c30b3332f5f99a3dc
yamori-masato <yamorimasato@gmail.com>
2021-06-25 04:51:01 +0900

    update Footer component

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+// TODO: それぞれのSNSアイコンの埋め込み
 const Footer = () => {
   return (
-    <footer className="bg-black w-full h-8 flex justify-center items-center">
-      <p className="text-white text-xxs">@2021geikosai</p>
+    <footer className="">
+      <div className="bg-white h-24 pt-2 flex flex-col justify-center items-center">
+        <p className="font-bold text-black">OFFICIAL SNS</p>
+        <ul className="flex gap-3">
+          <li>Line</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+        </ul>
+      </div>
+      <p className="bg-black h-8  flex justify-center items-center text-white text-xxs">@2021geikosai</p>
     </footer>
   );
 };
