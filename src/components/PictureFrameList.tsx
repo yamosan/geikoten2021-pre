03661267220a9d01278cd88f1dfe27760f03fd37
yamori-masato <yamorimasato@gmail.com>
2021-06-25 03:11:03 +0900

    add PictureFrameList component

---
+++ src/components/PictureFrameList.tsx
@@ -0,0 +1,21 @@
+import React, { VFC } from "react";
+import PictureFrame from "./parts/PictureFrame";
+
+type Props = {
+  departments: {
+    department: string;
+    manager: string;
+  }[];
+};
+
+const PictureFrameList: VFC<Props> = ({ departments }) => {
+  return (
+    <div className="grid grid-cols-2 justify-center justify-items-center gap-6 w-max">
+      {departments.map((v, i) => (
+        <PictureFrame title={v.department} subTitle={v.manager} key={i.toString()} />
+      ))}
+    </div>
+  );
+};
+
+export default PictureFrameList;
