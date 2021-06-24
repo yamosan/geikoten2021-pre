e1ba3aea39816d0782e4c1f21cfd82a52ea2c72c
yamori-masato <yamorimasato@gmail.com>
2021-06-24 14:14:25 +0900

    add Heading component

---
+++ src/components/parts/Heading.tsx
@@ -0,0 +1,20 @@
+import React, { VFC, ReactNode, DOMAttributes } from "react";
+
+type TagName = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+type Props = DOMAttributes<HTMLHeadingElement> & {
+  children: ReactNode;
+  as?: TagName;
+};
+
+const Heading: VFC<Props> = ({ children, as = "h2", ...rest }) => {
+  const Tag = as;
+
+  return (
+    <Tag {...rest} className="bg-white border-black border text-lg text-black text-center px-6 py-1 min-w-max">
+      {children}
+    </Tag>
+  );
+};
+
+export default Heading;
