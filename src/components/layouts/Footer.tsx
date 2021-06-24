43d4b2ed1c358731958fddfbc38c80b93a45586f
yamori-masato <yamorimasato@gmail.com>
2021-06-24 18:07:57 +0900

    fix Footer component

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-black absolute bottom-0 w-full h-8 flex justify-center items-center">
-      <p className="text-white text-xs">@2021geikosai</p>
+      <p className="text-white text-xxs">@2021geikosai</p>
     </footer>
   );
 };
