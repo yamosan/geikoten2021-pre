2cb196188ba1ba3ed9bc2ea8d88efe6f329d4545
yamori-masato <yamorimasato@gmail.com>
2021-06-24 20:10:11 +0900

    add FirstView component

---
+++ src/components/parts/FirstView.tsx
@@ -0,0 +1,26 @@
+import React, { VFC } from "react";
+import Image from "next/image";
+
+type Props = {
+  index: string;
+  title: string;
+};
+
+const FirstView: VFC<Props> = ({ index, title }) => {
+  return (
+    <div className="relative overflow-hidden">
+      <figure className="bg-gray w-full h-60 flex justify-center items-center">
+        <Image src="/img/page_thumbnail.png" alt="ページのサムネイル" layout="fill" className="object-cover" />
+      </figure>
+      <div className="absolute top-0 left-0 right-0 bottom-0 py-1 px-4 flex items-end">
+        <div className="w-full flex items-center gap-3">
+          {/* FIXME: genuineの高さが合わない */}
+          <span className="text-white text-4xl font-genuine pt-2.5">{index}</span>
+          <span className="text-white text-2xl font-bold">{title}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FirstView;
