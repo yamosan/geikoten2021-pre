1715adf7ff2995db000a5a07cdad97ce31e747f2
yamori-masato <yamorimasato@gmail.com>
2021-06-28 19:07:34 +0900

    fix props

--- src/components/PictureFrameList.tsx
+++ src/components/PictureFrameList.tsx
@@ -1,18 +1,16 @@
+import { Department } from "models/department";
 import React, { VFC } from "react";
 import PictureFrame from "./parts/PictureFrame";
 
 type Props = {
-  departments: {
-    department: string;
-    manager: string;
-  }[];
+  departments: Pick<Department, "name" | "manager">[];
 };
 
 const PictureFrameList: VFC<Props> = ({ departments }) => {
   return (
     <div className="grid grid-cols-2 justify-center justify-items-center gap-3.5">
       {departments.map((v, i) => (
-        <PictureFrame title={v.department} subTitle={v.manager} key={i.toString()} />
+        <PictureFrame title={v.name} subTitle={v.manager.class + "." + v.manager.name} key={i.toString()} />
       ))}
     </div>
   );
