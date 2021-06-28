e079bb4c6ef1225f0b6595fd47a39a5b89b70a7d
yamori-masato <yamorimasato@gmail.com>
2021-06-28 19:20:57 +0900

    add movie page

---
+++ src/pages/movie.tsx
@@ -0,0 +1,28 @@
+import Layout from "components/layouts";
+import FirstView from "components/parts/FirstView";
+import Section from "components/Section";
+import type { NextPage } from "next";
+
+const IndexPage: NextPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <FirstView index="02" title="ムービー" />
+    <div className="w-5/6 mx-auto">
+      <p className="text-black text-sm font-normal leading-6 pt-6">
+        映像部署が制作したムービーです。
+        <br />
+        <span className="highlight">見なきゃ損！</span>
+      </p>
+      <Section heading="コンセプトムービー" className="pt-12">
+        <div className="w-full h-44 bg-gray"></div>
+      </Section>
+      <Section heading="部署" className="pt-12">
+        <div className="w-full h-44 bg-gray"></div>
+      </Section>
+      <Section heading="CM" className="pt-12">
+        <div className="w-full h-44 bg-gray"></div>
+      </Section>
+    </div>
+  </Layout>
+);
+
+export default IndexPage;
