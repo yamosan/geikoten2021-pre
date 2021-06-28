6dcafae867933d03f8b37871a6debbb2931df6e3
yamori-masato <yamorimasato@gmail.com>
2021-06-28 19:12:33 +0900

    add departments page

--- src/pages/departments/index.tsx
+++ src/pages/departments/index.tsx
@@ -1,23 +1,63 @@
-import { GetStaticProps, NextPage } from "next";
-
-import { Department } from "models/department";
+import { GetStaticProps } from "next";
 import Layout from "components/layouts";
+import FirstView from "components/parts/FirstView";
+import TekibusyoLink from "components/TekibusyoLink";
+import QAndAPageLink from "components/QAndAPageLink";
+import Section from "components/Section";
+import PictureFrameList from "components/PictureFrameList";
+import type { NextPage } from "next";
+import type { Department } from "models/department";
 
 type Props = {
   items: Department[];
 };
 
-// export const getStaticProps: GetStaticProps<Props> = async () => {
-//   const items: Department[] = departments; // TODO: data/departments配下のjson全てを読み込み
-//   return { props: { items } };
-// };
+const _items: Department[] = [
+  {
+    id: 1,
+    name: "芸工祭長",
+    manager: {
+      name: "脛骨",
+      class: "3O",
+    },
+    qAndA: [
+      {
+        question: "どんなお仕事してますか？",
+        answers: [
+          {
+            content: "椅子に座って偉そうに皆に指示をしているよ！",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  // const items: Department[] = departments; // TODO: data/departments配下のjson全てを読み込み
+  return { props: { items: _items } };
+};
 
 const Departments: NextPage<Props> = ({ items }) => (
   <Layout title="Departments | Next.js + TypeScript Example">
-    <h1>Departments</h1>
-    {items.map((item) => (
-      <div key={item.id}>{item.name}</div>
-    ))}
+    <FirstView index="01" title="部署紹介" />
+    <div className="w-5/6 mx-auto pt-8">
+      <p className="text-black text-sm font-normal leading-6">
+        芸工祭には全部で19の部署長と幹部が居ます。
+        <br />
+        <span className="highlight">気になる部署</span>に所属して、みんなで芸工祭を盛り上げよう！
+      </p>
+      <div className="flex flex-col gap-3 pt-6">
+        <TekibusyoLink />
+        <QAndAPageLink />
+      </div>
+      <Section heading="幹部" className="pt-12">
+        <PictureFrameList departments={items} />
+      </Section>
+      <Section heading="部署" className="pt-12">
+        <PictureFrameList departments={items} />
+      </Section>
+    </div>
   </Layout>
 );
 
