8c337ab5be4857b0b7b985f3a7a74a4e2d0fcf0b
yamori-masato <yamorimasato@gmail.com>
2021-06-29 06:51:56 +0900

    use SnsLinks component

--- src/components/layouts/GlobalNavModal.tsx
+++ src/components/layouts/GlobalNavModal.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Link from "next/link";
+import SnsLinks from "components/SnsLinks";
 
 const GlobalNavModal = () => {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center">
-      <ul className="flex flex-col items-center gap-8 font-medium text-white text-2xl">
+    <div className="relative w-full h-full flex flex-col justify-center items-center">
+      <ul className="flex flex-col items-center gap-8 font-medium text-white text-2xl pb-24">
         <li>
           <Link href="/">
             <a>TOP</a>
@@ -31,6 +32,9 @@ const GlobalNavModal = () => {
           </Link>
         </li>
       </ul>
+      <div className="absolute bottom-0 w-full">
+        <SnsLinks />
+      </div>
     </div>
   );
 };
