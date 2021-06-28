cb5b06f0a8ea703fb8a85cd4a97fa3228e6eed5f
yamori-masato <yamorimasato@gmail.com>
2021-06-28 18:05:07 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import Layout from "components/Layout";
+import Layout from "components/layouts";
 
 const IndexPage: NextPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
