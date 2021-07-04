643c3fb08dc5a9ec0300fd1ede3f8d571420dc2f
yamori-masato <yamorimasato@gmail.com>
2021-07-04 20:12:22 +0900

    change to managers

--- src/components/PictureFrameList.tsx
+++ src/components/PictureFrameList.tsx
@@ -3,14 +3,20 @@ import React, { VFC } from "react";
 import PictureFrame from "./parts/PictureFrame";
 
 type Props = {
-  departments: Pick<Department, "name" | "manager">[];
+  departments: Pick<Department, "name" | "managers">[];
 };
 
 const PictureFrameList: VFC<Props> = ({ departments }) => {
   return (
     <div className="grid grid-cols-2 justify-center justify-items-center gap-3.5">
       {departments.map((v, i) => (
-        <PictureFrame title={v.name} subTitle={v.manager.class + "." + v.manager.name} key={i.toString()} />
+        <PictureFrame title={v.name} key={i.toString()}>
+          {v.managers.map((manager, j) => (
+            <h6 key={j.toString()} className="text-black font-bold text-xs">
+              {manager.class + "." + manager.name}
+            </h6>
+          ))}
+        </PictureFrame>
       ))}
     </div>
   );
