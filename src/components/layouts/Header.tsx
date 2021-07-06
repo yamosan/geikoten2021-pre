14db95fcea2c66474a8ec7fa4cf4b021ae2236b5
yamori-masato <yamorimasato@gmail.com>
2021-07-07 05:49:17 +0900

    add useBackfaceFixed hooks

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,4 +1,5 @@
 import React, { VFC, useState } from "react";
+import useBackfaceFixed from "hooks/useBackfaceFixed";
 import Link from "next/link";
 import Image from "next/image";
 import HamburgerMenu from "components/parts/HamburgerMenu";
@@ -10,24 +11,28 @@ const Header: VFC = () => {
     setIsOpen((state) => !state);
   };
 
+  useBackfaceFixed(isOpen);
+
   return (
-    <header className="w-full h-16 flex justify-between items-center fixed top-0 left-0 z-50 px-4 bg-transparent">
-      <div className="z-50">
-        <Link href="/">
-          <a className="flex items-center">
-            <Image src="/img/site_logo.png" alt="サイトロゴ" width={161} height={16} layout="fixed" />
-          </a>
-        </Link>
-      </div>
-      <nav className="z-40 flex items-center">
-        {isOpen && (
-          <div className="fixed inset-0 bg-primary">
-            <GlobalNavModal onLinkClick={toggleOpen} />
-          </div>
-        )}
-        <HamburgerMenu isOpen={isOpen} onClick={toggleOpen} />
-      </nav>
-    </header>
+    <>
+      <header className="w-full h-16 flex justify-between items-center fixed top-0 left-0 z-50 px-4 bg-transparent">
+        <div className="z-50">
+          <Link href="/">
+            <a className="flex items-center">
+              <Image src="/img/site_logo.png" alt="サイトロゴ" width={161} height={16} layout="fixed" />
+            </a>
+          </Link>
+        </div>
+        <nav className="z-40 flex items-center">
+          {isOpen && (
+            <div className="fixed inset-0 bg-primary">
+              <GlobalNavModal onLinkClick={toggleOpen} />
+            </div>
+          )}
+          <HamburgerMenu isOpen={isOpen} onClick={toggleOpen} />
+        </nav>
+      </header>
+    </>
   );
 };
 
