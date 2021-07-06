55ee9ab47a038433349ac2c2a77a7499267fb16d
yamori-masato <yamorimasato@gmail.com>
2021-07-07 03:30:03 +0900

    add link

--- src/components/DepartmentsList.tsx
+++ src/components/DepartmentsList.tsx
@@ -1,5 +1,6 @@
-import { Department } from "models/department";
 import React, { VFC } from "react";
+import Link from "next/link";
+import { Department } from "models/department";
 import PictureFrame from "./parts/PictureFrame";
 
 type Props = {
@@ -10,13 +11,17 @@ const PictureFrameList: VFC<Props> = ({ departments }) => {
   return (
     <div className="grid grid-cols-2 justify-center justify-items-center gap-3.5">
       {departments.map((v, i) => (
-        <PictureFrame title={v.name} key={i.toString()}>
-          {v.managers.map((manager, j) => (
-            <h6 key={j.toString()} className="text-black font-bold text-xs">
-              {manager.class + "." + manager.name}
-            </h6>
-          ))}
-        </PictureFrame>
+        <Link href={`/departments/${v.name}`} key={i.toString()}>
+          <a>
+            <PictureFrame title={v.name}>
+              {v.managers.map((manager, j) => (
+                <h6 key={j.toString()} className="text-black font-bold text-xs">
+                  {manager.class + "." + manager.name}
+                </h6>
+              ))}
+            </PictureFrame>
+          </a>
+        </Link>
       ))}
     </div>
   );
