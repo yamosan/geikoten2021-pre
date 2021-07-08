d8461b1d4f511248636b5f42a9668485a11c24f8
yamori-masato <yamorimasato@gmail.com>
2021-07-09 02:55:27 +0900

    add image

--- src/components/parts/PictureFrame.tsx
+++ src/components/parts/PictureFrame.tsx
@@ -4,15 +4,16 @@ import Image from "components/parts/Image";
 type Props = {
   title: string;
   children: ReactNode;
+  src: string;
 };
 
-const PictureFrame: VFC<Props> = ({ title, children }) => {
+const PictureFrame: VFC<Props> = ({ title, children, src }) => {
   return (
     <div className="flex flex-col items-center">
       <figure className="relative w-36 h-36 p-2.5 border border-black flex flex-col items-center ">
         <span className="absolute top-1 w-1 h-1 bg-black rounded-full"></span>
         <div className="relative w-full h-full flex justify-center items-center overflow-hidden">
-          <Image src="/img/400x250.png" alt="" layout="fill" className="object-cover bg-gray p-2" />
+          <Image src={src} alt="" layout="fill" className="object-cover bg-gray p-2" />
         </div>
       </figure>
       <div className="flex flex-col items-center">
