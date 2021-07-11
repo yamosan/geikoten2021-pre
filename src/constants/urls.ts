553f8b2db36d4106838db9e7ffb42fa9522eabd6
yamori-masato <yamorimasato@gmail.com>
2021-07-11 18:18:27 +0900

    add contents url

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -3,3 +3,10 @@ export const LINE_URL = "https://lin.ee/r9XIORr";
 export const LINE_ICON_URL = "https://scdn.line-apps.com/n/line_add_friends/btn/ja.png";
 export const TWITTER_URL = "https://twitter.com/geik0";
 export const INSTAGRAM_URL = "https://instagram.com/geikoten2021";
+
+// 芸工祭プレサイト
+export const SITE_DOMAIN = "http://geikousai-ncu.com";
+
+// コンテンツ urls
+export const TEKIBUSYO_CHART_URL = SITE_DOMAIN + "/chart";
+export const GAME_URL = SITE_DOMAIN + "/game";
