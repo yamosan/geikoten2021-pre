3987c4e7db9b3af6ba5bb603c771a5befe5e17ac
yamori-masato <yamorimasato@gmail.com>
2021-07-11 17:47:32 +0900

    use FadeInWhenVisible component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import News from "components/News";
 import ContentsNav from "components/ContentsNav";
 import Div100vh from "react-div-100vh";
 import { motion, AnimatePresence } from "framer-motion";
+import FadeInWhenVisible from "components/parts/FadeInWhenVisible";
 
 const IndexPage: NextPage = () => (
   <>
@@ -41,42 +42,48 @@ const IndexPage: NextPage = () => (
       </section>
 
       <section className="mt-16 flex flex-col items-center">
-        <h2 className="font-genuine text-primary tracking-wider text-5xl" style={{ lineHeight: 0.8 }}>
-          ABOUT
-        </h2>
-        <h4 className="font-bold text-black text-xs ">芸工展とは？</h4>
+        <FadeInWhenVisible>
+          <h2 className="font-genuine text-primary tracking-wider text-5xl text-center">ABOUT</h2>
+          <h4 className="font-bold text-black text-xs text-center">芸工展とは？</h4>
+        </FadeInWhenVisible>
         <div className="flex w-full items-center justify-between px-2 py-3">
           <div className="w-28 h-28 bg-gray"></div>
           <div className="w-28 h-28 bg-gray"></div>
           <div className="w-28 h-28 bg-gray"></div>
         </div>
-        <p className="w-10/12 font-bold text-black text-sm text-center leading-7">
-          芸工生が力を尽くして生み出した作品、見て思わず笑ってしまうステージ企画、オンラインだからこそ味わえるCGの世界など見どころ満載です！
-          <span className="inline-block highlight">芸工生全員で作る一つの作品</span>で、 <br />
-          コロナの時期だからこそ自分達の能力を
-          <br />
-          存分に活かして出来る芸工展です！
-          <br />
-          みんなで盛り上げよう！
-        </p>
+        <FadeInWhenVisible>
+          <p className="w-10/12 font-bold text-black text-sm text-center leading-7 mx-auto">
+            芸工生が力を尽くして生み出した作品、見て思わず笑ってしまうステージ企画、オンラインだからこそ味わえるCGの世界など見どころ満載です！
+            <span className="inline-block highlight">芸工生全員で作る一つの作品</span>で、 <br />
+            コロナの時期だからこそ自分達の能力を
+            <br />
+            存分に活かして出来る芸工展です！
+            <br />
+            みんなで盛り上げよう！
+          </p>
+        </FadeInWhenVisible>
       </section>
 
       <section className="my-16 flex flex-col items-center">
-        <h2 className="font-genuine text-primary tracking-wider text-5xl" style={{ lineHeight: 0.8 }}>
-          THEME
-        </h2>
-        <h4 className="font-bold text-black text-xs ">今年のテーマ</h4>
+        <FadeInWhenVisible>
+          <h2 className="font-genuine text-primary tracking-wider text-5xl text-center">THEME</h2>
+          <h4 className="font-bold text-black text-xs text-center">今年のテーマ</h4>
+        </FadeInWhenVisible>
         <div className="w-full py-3">
           <Image src="/img/theme.png" alt="今年のテーマ" layout="responsive" width={378} height={188} />
         </div>
         <div className="w-10/12 font-medium text-black text-sm text-center leading-7">
-          <p className="font-bold pt-4">
-            今年の芸工展のテーマにはカメラのフラッシュ機能に擬えて、芸工展を大学生の一つの大きな思い出としてフラッシュを焚いて、一人一人の人生に保存してほしいという思いを込めました。
-          </p>
-          <p className="font-bold pt-8">
-            また、「FUllUSH」には「輝く、駆け抜ける」という意味の「FLASH」や、「元気のある様」という意味の「FULL
-            FLUSH」という言葉も含まれています。芸工生の皆、大学生らしく元気に駆け抜けて行こう！
-          </p>
+          <FadeInWhenVisible>
+            <p className="font-bold pt-4">
+              今年の芸工展のテーマにはカメラのフラッシュ機能に擬えて、芸工展を大学生の一つの大きな思い出としてフラッシュを焚いて、一人一人の人生に保存してほしいという思いを込めました。
+            </p>
+          </FadeInWhenVisible>
+          <FadeInWhenVisible>
+            <p className="font-bold pt-8">
+              また、「FUllUSH」には「輝く、駆け抜ける」という意味の「FLASH」や、「元気のある様」という意味の「FULL
+              FLUSH」という言葉も含まれています。芸工生の皆、大学生らしく元気に駆け抜けて行こう！
+            </p>
+          </FadeInWhenVisible>
         </div>
       </section>
 
