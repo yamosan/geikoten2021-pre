e97db494b8f0706fe8328f4d6cf154f3297d77ad
yamori-masato <yamorimasato@gmail.com>
2021-07-11 18:22:46 +0900

    update ContentsNav

--- src/components/ContentsNav.tsx
+++ src/components/ContentsNav.tsx
@@ -1,48 +1,41 @@
 import React from "react";
 import Link from "next/link";
 import Card from "components/parts/Card";
+import { GAME_URL, TEKIBUSYO_CHART_URL } from "constants/urls";
 
 const CONTENTS = [
   {
     title: "部署紹介",
     href: "/departments",
     imgSrc: "/img/contents/departments/thumbnail.jpg",
-    isDisabled: false,
   },
   {
     title: "適部署チャート",
-    href: "/",
+    href: TEKIBUSYO_CHART_URL,
     imgSrc: "/img/contents/tekibusyo/thumbnail.jpg",
-    isDisabled: true,
   },
   {
     title: "ムービー",
     href: "/movie",
     imgSrc: "/img/contents/movie/thumbnail.jpg",
-    isDisabled: false,
   },
   {
     title: "ゲーム",
-    href: "/",
+    href: GAME_URL,
     imgSrc: "/img/contents/game/thumbnail.jpg",
-    isDisabled: true,
   },
 ];
 
 const CardList = () => {
   return (
     <nav className="grid grid-cols-2 justify-center justify-items-center gap-6 w-max">
-      {CONTENTS.map(({ title, href, imgSrc, isDisabled }, i) => {
-        return !isDisabled ? (
-          <Link href={href} key={title}>
-            <a>
-              <Card title={title} batch={(i + 1).toString().padStart(2, "0")} imgSrc={imgSrc} key={i.toString()} />
-            </a>
-          </Link>
-        ) : (
-          <Card title={title} batch={(i + 1).toString().padStart(2, "0")} imgSrc={imgSrc} key={i.toString()} />
-        );
-      })}
+      {CONTENTS.map(({ title, href, imgSrc }, i) => (
+        <Link href={href} key={title}>
+          <a>
+            <Card title={title} batch={(i + 1).toString().padStart(2, "0")} imgSrc={imgSrc} key={i.toString()} />
+          </a>
+        </Link>
+      ))}
     </nav>
   );
 };
