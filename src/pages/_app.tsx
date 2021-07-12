b6d4a765c147dd1792ee14e87e5c5af6b156d615
yamori-masato <yamorimasato@gmail.com>
2021-07-13 04:40:00 +0900

    add simple page transitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,24 @@
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 import "styles/globals.css";
+import { AnimatePresence, motion } from "framer-motion";
 import Layout from "components/layouts";
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps, router }: AppProps) => {
+  console.log(router);
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          key={router.route}
+          transition={{ duration: 0.3 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 };
