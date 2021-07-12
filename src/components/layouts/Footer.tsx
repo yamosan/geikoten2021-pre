99e0715ebc6b4b6639e2619e6c0e898d4eab2d29
yamori-masato <yamorimasato@gmail.com>
2021-07-13 04:50:09 +0900

    fix

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="">
-      <p className="bg-black h-8  flex justify-center items-center text-white text-xxs">@2021geikoten</p>
+      <p className="bg-black h-8  flex justify-center items-center text-white text-xxs">@2021 GEIKO TEN</p>
     </footer>
   );
 };
