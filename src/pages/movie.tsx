09a6387ba2216cd624adb866814145c41ba29007
yamori-masato <yamorimasato@gmail.com>
2021-07-13 14:41:18 +0900

    fix movie page

--- src/pages/movie.tsx
+++ src/pages/movie.tsx
@@ -5,19 +5,16 @@ import type { NextPage } from "next";
 const IndexPage: NextPage = () => (
   <>
     <FirstView index="03" title="ムービー" src="/img/contents/movie/hero.png" />
-    <div className="w-5/6 mx-auto pb-10">
+    <div className="w-5/6 mx-auto pb-20">
       <p className="text-black text-sm font-medium leading-6 pt-6">
         映像部署が制作したムービーです。
         <br />
         <span className="highlight">見なきゃ損！</span>
       </p>
-      <Section heading="コンセプトムービー" className="pt-12">
+      <Section heading="コンセプトムービー" className="pt-8">
         <div className="w-full h-44 bg-gray"></div>
       </Section>
-      <Section heading="部署" className="pt-12">
-        <div className="w-full h-44 bg-gray"></div>
-      </Section>
-      <Section heading="CM" className="pt-12">
+      <Section heading="部署長紹介" className="pt-16">
         <div className="w-full h-44 bg-gray"></div>
       </Section>
     </div>
