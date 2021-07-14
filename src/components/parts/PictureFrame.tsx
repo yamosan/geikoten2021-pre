2cf2047410e132e5e52f49a7527a6f3a3848f2a8
yamori-masato <yamorimasato@gmail.com>
2021-07-14 12:23:29 +0900

    fix

--- src/components/parts/PictureFrame.tsx
+++ src/components/parts/PictureFrame.tsx
@@ -17,7 +17,7 @@ const PictureFrame: VFC<Props> = ({ title, children, src, start }) => {
       controls.start({
         rotate: [0, -3, -3, 3, -2, 1, -1, 0.5, -0.2, 0.1, 0],
         transition: {
-          duration: 5,
+          duration: 3,
         },
       });
     }
@@ -25,7 +25,12 @@ const PictureFrame: VFC<Props> = ({ title, children, src, start }) => {
 
   return (
     <div className="flex flex-col items-center">
-      <motion.div initial={{ rotate: 0 }} animate={controls} style={{ originX: 0.5, originY: 0 }}>
+      <motion.div
+        initial={{ rotate: 0 }}
+        animate={controls}
+        style={{ originX: 0.5, originY: 0 }}
+        className="stop-flickering"
+      >
         <figure className="relative w-36 h-36 p-2.5 border border-black bg-white flex flex-col items-center">
           <span className="absolute top-1 w-1 h-1 bg-black rounded-full"></span>
           <div className="relative w-full h-full flex justify-center items-center overflow-hidden">
