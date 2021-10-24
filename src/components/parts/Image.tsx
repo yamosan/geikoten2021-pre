48b4f70b94dfb77573da3f08270b7718192e7047
yamo <68507315+yamosan@users.noreply.github.com>
2021-10-24 19:11:14 +0900

    Update Image.tsx

--- src/components/parts/Image.tsx
+++ src/components/parts/Image.tsx
@@ -3,9 +3,11 @@ import NextImage from "next/image";
 
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH;
 
+const customLoader = ({ src }) => src;
+
 const Image: typeof NextImage = (props) => {
   const url = props.src?.startsWith("/") ? `${basePath || ""}${props.src}` : props.src;
-  return <NextImage {...props} src={url} />;
+  return <NextImage {...props} src={url} loader={customLoader} unoptimized />;
 };
 
 export default Image;
